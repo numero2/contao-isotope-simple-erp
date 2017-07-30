44c27f5aec2782a01d0142ffdb7d81e99321ebdc
Benny Born <git@bennyborn.de>
2017-07-30 14:25:23 +0200

    removed comment

--- config/config.php
+++ config/config.php
@@ -18,5 +18,3 @@
 $GLOBALS['ISO_HOOKS']['postCheckout'][] = array('numero2\IsotopeSimpleERP\SimpleERP', 'updateProductCount');
 $GLOBALS['ISO_HOOKS']['addProductToCollection'][] = array('numero2\IsotopeSimpleERP\SimpleERP', 'checkQtyForCollection');
 $GLOBALS['TL_HOOKS']['getSystemMessages'][] = array('numero2\IsotopeSimpleERP\SimpleERP', 'getSystemMessages');
-
-// TODO: prevent adding more to cart than in stock
